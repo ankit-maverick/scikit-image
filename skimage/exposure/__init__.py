1ac65f8fb09e944d5c81e25ad1ea891ad8a03954
Ankit Agrawal <aaaagrawal@iitb.ac.in>
2013-04-16 06:38:11 +0530

    Added a function for Gamma Correction

--- skimage/exposure/__init__.py
+++ skimage/exposure/__init__.py
@@ -1,3 +1,3 @@
 from .exposure import histogram, equalize, equalize_hist
-from .exposure import rescale_intensity, cumulative_distribution
+from .exposure import rescale_intensity, cumulative_distribution, gammaCorrect
 from ._adapthist import equalize_adapthist
