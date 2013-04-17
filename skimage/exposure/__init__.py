e8534f2e15b6f80004b38455c824288f55b9c43b
Ankit Agrawal <aaaagrawal@iitb.ac.in>
2013-04-18 05:26:10 +0530

    Prevented info loss, implemented Log and Sigmoid Correction

--- skimage/exposure/__init__.py
+++ skimage/exposure/__init__.py
@@ -1,3 +1,3 @@
 from .exposure import histogram, equalize, equalize_hist
-from .exposure import rescale_intensity, cumulative_distribution, gammaCorrect
+from .exposure import rescale_intensity, cumulative_distribution, correct
 from ._adapthist import equalize_adapthist
