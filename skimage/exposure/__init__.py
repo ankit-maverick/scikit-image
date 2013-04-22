fdac65d2d282159fde060d0bfd6ee55a98d9740b
Ankit Agrawal <aaaagrawal@iitb.ac.in>
2013-04-22 13:26:25 +0530

    Separating correction methods into different functions

--- skimage/exposure/__init__.py
+++ skimage/exposure/__init__.py
@@ -1,3 +1,5 @@
 from .exposure import histogram, equalize, equalize_hist
-from .exposure import rescale_intensity, cumulative_distribution, correct
+from .exposure import (rescale_intensity, cumulative_distribution,
+	rescale_intensity_gamma, rescale_intensity_logarithmic,
+	rescale_intensity_sigmoid)
 from ._adapthist import equalize_adapthist
